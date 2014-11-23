7098bf20f69d45c2012d31312542e84f15a0a54f
teppeis <teppeis@gmail.com>
2014-11-23 10:53:24 +0900

    separate node-adaptor.js

---
+++ test/node-adaptor.js
@@ -0,0 +1,4 @@
+'use strict';
+
+global.htmlspecialchars = require('../');
+global.assert = require('power-assert');
--- test/test.js
+++ test/test.js
@@ -1,34 +1,31 @@
 'use strict';
 
-var assert = require('power-assert');
-var sut = require('../');
-
 describe('htmlspecialchars', function() {
     it('escapes "&" to "&amp;"', function() {
-        assert(sut('&') === '&amp;');
+        assert(htmlspecialchars('&') === '&amp;');
     });
     it('escapes "<" to "&lt;"', function() {
-        assert(sut('<') === '&lt;');
+        assert(htmlspecialchars('<') === '&lt;');
     });
     it('escapes ">" to "&gt;"', function() {
-        assert(sut('>') === '&gt;');
+        assert(htmlspecialchars('>') === '&gt;');
     });
     it('escapes \'"\' to "&quot;"', function() {
-        assert(sut('"') === '&quot;');
+        assert(htmlspecialchars('"') === '&quot;');
     });
     it('escapes "\'" to "&#039;"', function() {
-        assert(sut("'") === '&#039;');
+        assert(htmlspecialchars("'") === '&#039;');
     });
     it('escapes all special chars', function() {
-        assert(sut("<&<&") === '&lt;&amp;&lt;&amp;');
+        assert(htmlspecialchars("<&<&") === '&lt;&amp;&lt;&amp;');
     });
     it('returns an empty string for null', function() {
-        assert(sut(null) === '');
+        assert(htmlspecialchars(null) === '');
     });
     it('returns an empty string for undefined', function() {
-        assert(sut(null) === '');
+        assert(htmlspecialchars(null) === '');
     });
     it('stringify non-string value', function() {
-        assert(sut(1) === '1');
+        assert(htmlspecialchars(1) === '1');
     });
 });
